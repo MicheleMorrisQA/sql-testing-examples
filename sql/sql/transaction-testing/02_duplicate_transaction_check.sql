802e4278459274b1cb7244e8213f140b4dd00c83
Michele Morris <meesh610@gmail.com>
2026-09-10 12:48:46 -0400

    Add QA test for duplicate transaction IDs
    Added a QA test to check for duplicate transaction IDs in the transactions table.

--- sql/sql/transaction-testing/02_duplicate_transaction_check.sql
+++ sql/sql/transaction-testing/02_duplicate_transaction_check.sql
@@ -1,1 +1,9 @@
+-- QA Test: Identify Duplicate Transactions
+-- Purpose: Verify that transaction IDs are unique.
 
+SELECT
+    transaction_id,
+    COUNT(*) AS transaction_count
+FROM transactions
+GROUP BY transaction_id
+HAVING COUNT(*) > 1;
